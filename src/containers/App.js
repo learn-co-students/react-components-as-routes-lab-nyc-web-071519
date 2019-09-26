886e7c8ed0bd34c16ff06c0b5ef76d41c212b434
Andrew Santos <asantos@alumni.nd.edu>
2019-09-26 11:50:48 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,9 +11,21 @@ import Movies from '../components/Movies';
 
 
 const App = (props) => {
+
+  // const NavBar = () => {
+    
+    
+  // }
+
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={Movies}/>
+        <Route exact path="/directors" component={Directors}/>
+        <Route exact path="/actors" component={Actors}/>
+      </div>
     </Router>
   );
 };
